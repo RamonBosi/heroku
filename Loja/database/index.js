1b679988d5d96442cded27c12e1f42de1d4dffde
Ramon Bosi <ramombosi2015@gmail.com>
2022-07-25 16:13:08 -0300

    Update index.js

--- Loja/database/index.js
+++ Loja/database/index.js
@@ -5,7 +5,8 @@ const database = new Pool({
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    ssl: { rejectUnauthorized: process.env.DATABASE_REJECT_UNAUTHORIZED}
 })
 
 module.exports = database
